b982bd7e3b36fbecc9402cd073f0b435470c0c4c
amisha patel <ami.patel2410@gmail.com>
2017-11-05 11:05:00 -0500

    create another component Hobbies

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const Component = React.Component;
+
 
 const userData = {
     name: "Joe",
